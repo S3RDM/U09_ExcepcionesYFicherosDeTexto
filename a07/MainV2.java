808a2853e98bb2d3d50a2796d66421f78f95c09d
S3RDM <legochecho@gmail.com>
2026-04-16 09:41:17 +0200

    a07

--- a07/MainV2.java
+++ a07/MainV2.java
@@ -1,27 +1,30 @@
 package a07;
 import java.io.*;
 
-public class Main {
+public class MainV2 {
     static final String fichero = "C:\\dam\\prog\\code\\fichero.txt";
         
     public static void main(String[] args){
         BufferedWriter out = null;
+        BufferedWriter out2 = null;
         try{
             out = new BufferedWriter(new FileWriter (fichero));
+            out2 = new BufferedWriter(new FileWriter (fichero, true));
             String linea1 = "En un lugar de La Mancha,";
             String linea2 = "de cuyo nombre no quiero acordarme";
             
             for(int i = 0; i < linea1.length(); i++){
                 out.write(linea1.charAt(i));
             }
-            out.newLine();
-            out.write(linea2);
+            out2.newLine();
+            out2.write(linea2);
         }catch(IOException e){
             System.out.println(e);
         }finally{
             if(out != null){
                 try{
                     out.close();
+                    out2.close();
                 }catch(IOException e){
                     System.out.println(e);
                 }
